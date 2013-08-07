ed1213a250a138b96308d01a95846160683dc9c0
JDunford1992 <s3330661@student.rmit.edu.au>
2013-08-07 20:17:09 +1000

    Update try1.php

--- try1.php
+++ try1.php
@@ -1,43 +1,36 @@
-<head>
-
-</head>
+<html>
+<head></head>
 
 <body>
-	<div>
 
-		<div>
-		</div>
+	<div id="main">
+			<h1>Welcome to the Alpha Tool</h1>
+			<p>- This is an RMIT Student Project for WDA (Sem-2 2013) -</p>
 
-		<div id="main">
-			<h1>Welcome to the Expedition Overview Alpha Tool</h1>
-			<p id="home">- This is an RMIT Student Project for Web/Internet Projects (Sem-1 2013) -</p>
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
 
-			<h3>
-				<a href="resourceComp.php"><img src="images/homeimage1.png" alt="homeStats1"/></a>
-				<a href="resourceComp.php"><img src="images/homeimage2.png" alt="homeStats2"/></a>
-				<a href="resourceComp.php"><img src="images/homeimage3.png" alt="homeStats3"/></a>
-			</h3>
+				<select value="tableName">
+					<?php
 
-			<p id="homeDesc">- Select An Image to View Data or Browse though the Navigation Tabs. -</p>
+					while($row = mysql_fetch_row($result)) {
+						$tableName = $row[0];
+						echo '<option value="$tableName">$tableName</option>';
+					} ?>
+				</select>
+
+				<?php // you will need another drop down list here 
+
+				echo '<option value="$tableName">$tableName</option>';
+
+				?>
+
+				<input type="submit" name="submit" value="Run Query">
+
+			</form>
 
 			<h3><i>-Comments Created By Jasio Dunford</i></h3>
 		</div>
-
-		<div id="footer">
-
-			<div id="validate">
-				<a href="http://validator.w3.org/check?uri=referer">
-					<img src="http://www.w3.org/Icons/valid-xhtml10" 
-					alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
-
-					<a href="http://jigsaw.w3.org/css-validator/check/referer">
-						<img style="border:0;width:88px;height:31px" 
-						src="http://jigsaw.w3.org/css-validator/images/vcss"
-						alt="Valid CSS!" /></a>
-					</div>
-
-					<?php include('footer.php'); ?>
-				</div>
-			</div>
-		</body>
-	</html>
+		
+	</body>
+<footer></footer>
+</html>

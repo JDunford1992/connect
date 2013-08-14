cf831980f41b0df01caa6a3cd5e463da3cdad922
JDunford1992 <s3330661@student.rmit.edu.au>
2013-08-14 19:20:26 +1000

    Update partB_s3330661.php

--- partB_s3330661.php
+++ partB_s3330661.php
@@ -10,6 +10,18 @@
 
 <body>
 
+	<?php
+require_once('db.php');
+if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
+echo 'Could not connect to mysql on ' . DB_HOST . '\n';
+exit; }
+if(!mysql_select_db(DB_NAME, $dbconn)) {
+echo 'Could not user database ' . DB_NAME . '\n'; echo mysql_error() . '\n';
+exit;
+}
+?>
+
+
 	<div id="main">
 		<h1>Welcome to the Alpha Tool</h1>
 		<p>- This is an RMIT Student Project for WDA (Sem-2 2013) -</p>

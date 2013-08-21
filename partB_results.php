08da7759d4cfe0ca63b8f9e2444997d3d94bca6f
JDunford1992 <s3330661@student.rmit.edu.au>
2013-08-21 19:55:04 +1000

    Update partB_results.php

--- partB_results.php
+++ partB_results.php
@@ -96,6 +96,7 @@
   wine.year, winery.winery_name, region.region_name, inventory.cost, inventory.on_hand
   FROM winery, wine, wine_variety, region, inventory, grape_variety, items
   WHERE winery.winery_id = wine.winery_id
+  AND winery.region_id = region.region_id
   AND wine.wine_id = inventory.wine_id
   AND wine.wine_id = items.wine_id
   AND wine.wine_id = wine_variety.wine_id
@@ -115,33 +116,33 @@
     $query .= " AND winery_name = '{$nameWinery}'";
   }
 
-  if (isset($region) && $region != "All") {
-    $query .= " AND region = '{$region}'";
-  }
+  // if (isset($region) && $region != "All") {
+  //   $query .= " AND region = '{$region}'";
+  // }
 
-  if (isset($grapeVariety) && $grapeVariety != 1) {
-    $query .= " AND variety = '{$grapeVariety}'";
-  }
+  // if (isset($grapeVariety) && $grapeVariety != 1) {
+  //   $query .= " AND variety = '{$grapeVariety}'";
+  // }
 
-  if (isset($yearLow) && $yearLow != "All") {
-    $query .= " AND year >= '{$yearLow}'";
-  }
+  // if (isset($yearLow) && $yearLow != "All") {
+  //   $query .= " AND year >= '{$yearLow}'";
+  // }
 
-  if (isset($yearMax) && $yearMax != "All") {
-    $query .= " AND year <= '{$yearMax}'";
-  }
+  // if (isset($yearMax) && $yearMax != "All") {
+  //   $query .= " AND year <= '{$yearMax}'";
+  // }
 
-  if (isset($costMin) && $costMin != "All") {
-    $query .= " AND cost >= '{$costMin}'";
-  }
+  // if (isset($costMin) && $costMin != "All") {
+  //   $query .= " AND cost >= '{$costMin}'";
+  // }
 
-  if (isset($costMax) && $costMax != "All") {
-    $query .= " AND cost <= '{$costMax}'";
-  }
+  // if (isset($costMax) && $costMax != "All") {
+  //   $query .= " AND cost <= '{$costMax}'";
+  // }
 
-  if (isset($minStock) && $minStock != "All") {
-    $query .= " AND on_hand >= '{$minStock}'";
-  }
+  // if (isset($minStock) && $minStock != "All") {
+  //   $query .= " AND on_hand >= '{$minStock}'";
+  // }
 
   // IF STEMENTS SHOULD WORK FINE HERE IF CORRECT
 
